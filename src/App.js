b840e8f9fd0ff6362a058a7e6c276e976436ac1d
edu <eddiberenguer@gmail.com>
2019-10-26 09:50:01 +0200

    add Logo and animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
-import {ListOfPhotoCards} from "./components/ListOfPhotoCards";
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
+
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
